9261bbbcf3945f1ef95d64065c7c0fbf993e3ba5
Winson Quon <winguy2003@gmail.com>
2020-02-22 10:40:23 -0500

    test commit

--- public/service-worker.js
+++ public/service-worker.js
@@ -4,14 +4,17 @@ var urlsToCache = [
   '/',
   '/styles.css',
   '/index.js',
-  '/manifest.json',
   '/db.js',
+  '/manifest.json',
+  '/service-worker.js',
+  '/favicon.ico',
   '/icons/icon-192x192.png',
   '/icons/icon-512x512.png'
 ];
 
 
 self.addEventListener('install', function(event) {
+  console.info('Event: Install');
   // Perform install steps
   event.waitUntil(
     caches.open(CACHE_NAME)
@@ -20,9 +23,30 @@ self.addEventListener('install', function(event) {
         return cache.addAll(urlsToCache);
       })
   );
+  self.skipWaiting();
 });
 
+// activate
+self.addEventListener("activate", function(evt) {
+  console.info('Event: Activate');
+  evt.waitUntil(
+    caches.keys().then(keyList => {
+      return Promise.all(
+        keyList.map(key => {
+          if (key !== CACHE_NAME) {
+            console.log("Removing old cache data", key);
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+/*
 self.addEventListener("fetch", function(event) {
+  console.info('Event: Fetch');
   event.respondWith(
     fetch(event.request).catch(function() {
       return caches.match(event.request).then(function(response) {
@@ -31,6 +55,21 @@ self.addEventListener("fetch", function(event) {
         } else if (event.request.headers.get("accept").includes("text/html")) {
           return caches.match("/index.html");
         }
+      });
+    })
+  );
+});
+*/
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((r) => {
+      console.log('[Service Worker] Fetching resource: '+e.request.url);
+      return r || fetch(e.request).then((response) => {
+        return caches.open(CACHE_NAME).then((cache) => {
+          console.log('[Service Worker] Caching new resource: '+e.request.url);
+          cache.put(e.request, response.clone());
+          return response;
+        });
       });
     })
   );
